9775b678faa5a3d8b8b803a0bf8b164d985cc849
karpenko <karpenko@reshenie-soft.ru>
2022-03-08 22:21:58 +0300

    Delete mapping for delete employee;

--- src/main/java/ru/se4oev/spring/rest/controller/MyRestController.java
+++ src/main/java/ru/se4oev/spring/rest/controller/MyRestController.java
@@ -48,4 +48,14 @@ public class MyRestController {
         return employee;
     }
 
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null)
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
+
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + " was deleted";
+    }
+
 }
