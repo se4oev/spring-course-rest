b79993124a371435fa435f5b32d549018e37cfc4
karpenko <karpenko@reshenie-soft.ru>
2022-03-08 22:01:51 +0300

    GLobal handling exceptions;

--- src/main/java/ru/se4oev/spring/rest/controller/MyRestController.java
+++ src/main/java/ru/se4oev/spring/rest/controller/MyRestController.java
@@ -1,10 +1,7 @@
 package ru.se4oev.spring.rest.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.se4oev.spring.rest.entity.Employee;
-import ru.se4oev.spring.rest.exception_handling.EmployeeIncorrectData;
 import ru.se4oev.spring.rest.exception_handling.NoSuchEmployeeException;
 import ru.se4oev.spring.rest.service.EmployeeService;
 
@@ -37,22 +34,6 @@ public class MyRestController {
             throw new NoSuchEmployeeException("There is no employee with id = " + id + " in database");
 
         return employeeService.getEmployee(id);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 
 }
